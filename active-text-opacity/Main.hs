856bd1c2ec9032e6cca6bcf08dbcf159f49ff3b5
Nicholas Shaheed <nshaheed@sulawesi.ittc.ku.edu>
2014-07-28 12:23:52 -0500

    Added opacity example

---
+++ active-text-opacity/Main.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Data.Active
+import Data.Fixed
+import Graphics.Blank
+import Control.Concurrent
+
+main :: IO ()
+main = blankCanvas 3000 $ \ context -> do
+     loop context (0 :: Time)
+
+loop :: DeviceContext -> Time -> IO ()
+loop context n = do
+        let x = mkActive 0 1 (rotateSquare context)
+        loopWorker x n
+
+
+loopWorker :: Active (IO ()) -> Time -> IO ()
+loopWorker x n = do runActive x n
+                    threadDelay (75 * 1000)	
+                    loopWorker x $ mod' (n + 0.01) 2
+
+rotateSquare :: DeviceContext -> Time -> IO ()
+rotateSquare context n =
+          send context $ do
+            let (w,h) = (width context, height context)
+            clearRect (0,0,w,h)
+            let n' = if n <= 1 then n else 2 - n  -- Logic for fade in and fade out
+            globalAlpha $ fromTime n'
+            font "40pt Calibri"
+            fillText("Opacity",50,50)
